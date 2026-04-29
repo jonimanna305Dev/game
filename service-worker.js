3f5e0189f4c838c9c8748601848f6d8faaed9583
Md Joni Islam <jonimanna305@gmail.com>
2026-04-30 00:45:10 +0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,52 @@
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open("xo-cache").then((cache) => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./main.js",
-        "./manifest.json",
-        "./icon.png"
-      ]);
-    })
-  );
+const CACHE_NAME = "novacore-app-v1";
+const urlsToCache = [
+  "/",
+  "./index.html",
+  "./about.html",
+  "./services.html",
+  "./blog.html",
+  "./contact.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Install
+self.addEventListener("install", event => {
   self.skipWaiting();
-});
-
-self.addEventListener("activate", () => {
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((res) => {
-      return res || fetch(event.request);
-    })
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(() => console.log("SW Registered"));
+}
